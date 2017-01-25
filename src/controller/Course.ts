287e59357f86e7d33992c86a7ec4df2c92b807da
jpeng06 <awv006@gmail.com>
2017-01-24 17:00:19 -0800

    Finished pre-load for dataset.

---
+++ src/controller/Course.ts
@@ -0,0 +1,57 @@
+/**
+ * Created by John on 2017-01-20.
+ */
+export default class Course
+{
+    //The department that offered the course.
+    private courses_dept: string;
+    //The course number (will be treated as a string (e.g., 499b)).
+    private courses_id: string;
+    //The average of the course offering.
+    private courses_avg: number;
+    //The instructor teaching the course offering.
+    private courses_instructor: string;
+    //The name of the course.
+    private courses_title: string;
+    //The number of students that passed the course offering.
+    private courses_pass: number;
+    //The number of students that failed the course offering.
+    private courses_fail: number;
+    //The number of students that audited the course offering.
+    private courses_audit: number;
+
+    constructor(dept: string, id:string)
+    {
+        this.courses_dept = dept;
+        this.courses_id = id;
+    }
+
+    pass(num: number)
+    {
+        this.courses_pass = num;
+    }
+
+    fail(num: number)
+    {
+        this.courses_fail = num;
+    }
+    audit(num: number)
+    {
+        this.courses_audit = num;
+    }
+
+    average(num: number)
+    {
+        this.courses_avg = num;
+    }
+
+    instrctor(name: string)
+    {
+        this.courses_instructor = name;
+    }
+
+    title(title: string)
+    {
+        this.courses_title = title;
+    }
+}
--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -4,12 +4,18 @@
 import {IInsightFacade, InsightResponse, QueryRequest} from "./IInsightFacade";
 
 import Log from "../Util";
-
+import DataList from "./DataList";
+import {isUndefined} from "util";
+import {error} from "util";
+let JSZip = require("jszip");
 
 export default class InsightFacade implements IInsightFacade {
 
+    private set:  Array<DataList>;
+
     constructor() {
         Log.trace('InsightFacadeImpl::init()');
+        this.set = [];
     }
 
 
@@ -44,29 +50,75 @@ export default class InsightFacade implements IInsightFacade {
      */
     addDataset(id: string, content: string): Promise<InsightResponse> {
         const instance = this;
+        //code will be used for fulfill only
+        let code: number = 0;
 
         return new Promise(function (fulfill, reject) {
 
             if (!(instance.isBase64(content)))
                 reject({code: 400, body: {"error": "Content Not Base64 Encoded"}});
 
-            let decoded: any = instance.decode(content);
+
+            //step1: decode base64 content to readable json object
+            let step1 = instance.decode(content).then(function (decoded) {
+
+                console.log(decoded);
 
 
-            var keys: any=[];
+                for (var key in decoded)
+                {
+                    if (decoded.hasOwnProperty(key))
+                    {
+                        //console.log(key + " = " + decoded[key]);
+                        let things = decoded[key];
+                        for (var inner in things)
+                        {
+                            if (key.hasOwnProperty(inner))
+                            {
+                                //console.log(inner + "=" +things[inner]);
+                            }
+                        }
+                    }
+                }
+
+            }).catch(function (err) {
+                console.log(err);
+            });
+
+            //console.log(decoded)
+
+            if (instance.isAdded(id))
+            {
+                code = 201;
+                //remove then add again if already exits
+                instance.removeDataset(id).catch(function(err) {
+                    reject({code: 400, body: {"error": "Deletion error"}})
+                });
+            }
+            else
+            {
+                code = 204;
+            }
+
+            /*var keys: any=[];
             var values: any=[];
             let i=0;
+
+
             for (var key in decoded[0]) {
                 keys[i]= key;
                 values[i]=content[0][key];
                 i=i+1;
             }
             console.log(keys);
-            console.log(values);
+            console.log(values);*/
 
 
-
-            fulfill( {code: 201, body: {}} );
+            Promise.all([step1]).then(function() {
+                fulfill( {code: code, body: {}} );
+            }).catch(function (err) {
+                console.log(err);
+            })
         });
     }
 
@@ -130,25 +182,98 @@ export default class InsightFacade implements IInsightFacade {
      */
     isBase64(input: string): boolean
     {
+        if (isUndefined(input) || input === "")
+            return false;
         //base64 should be multiple of 4 byte string
         if (input.length % 4 !== 0)
             return false;
         //base64 string ends with "="
-        if (input.charAt(input.length - 1) !== "=")
-            return false;
+        /*if (input.charAt(input.length - 1) !== "=")
+            return false;*/
 
         return true;
     }
      /**
-     * decodes a base64 string to JSON object
+     * decodes base64 dataset to JSON object
      *
      * @param input  given string needs to be decoded
      * @return JSON object
      */
-    decode(input: string): any
+    decode(input: string): Promise<any>{
+         let instance = this;
+
+        return new Promise( function (fulfill, reject) {
+
+
+            //we need to convert the data back to buffer
+            var buffer = new Buffer(input, 'base64');
+
+             instance.load(buffer)
+                 .then(function (okay: any)
+                 {
+                     let content: string = "";
+                     console.log("before");
+
+                     for (var filename in okay.files) {
+                         //inner promise is returned
+                         var readfile: Promise<any> = okay.file(filename).async("string")
+                             .then(function success(text: string) {
+
+                                 //console.log(text);
+                                 var buffer = new Buffer(text);
+                                 text = buffer.toString();
+                                 content = content + text;
+                                 //console.log(content);
+                                 //console.log('for loop');
+                             });
+                     }
+                     //console.log("fulfill");
+                     Promise.all([readfile]).then( function () {
+                         fulfill(content);
+                     });
+                 }).catch(function (err) {
+                 console.log(err);
+                 reject(null);
+                });
+
+         //console.log('gdfgdf');
+         //return pro;
+        /*console.log("input = " +input);
+        var b:string = new Buffer(input, 'base64').toString();
+        return JSON.parse(b);*/
+        //return null;
+        });
+    }
+
+    /**
+     * check if dataset has such id
+     *
+     * @param id given id that should be searched for
+     * @return true if such id exits in dataset. false otherwise
+     */
+    isAdded(id: string): boolean
     {
-        var b = new Buffer(input, 'base64');
-        input = b.toString();
-        return JSON.parse(input);
+        for (let i of this.set)
+        {
+            if (i.getID() === id)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    load(buffer: any): Promise<any>
+    {
+        return new Promise(function(fulfill, reject)
+        {
+            let zip = new JSZip();
+            zip.loadAsync(buffer).then(function (okay: any) {
+                fulfill(okay);
+            }).catch(function (err: any) {
+                reject(err);
+            });
+        });
     }
 }
