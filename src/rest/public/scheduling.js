2445f6a5d0031facccaed5e5bac26707c2aa4188
jpeng06 <awv006@gmail.com>
2017-03-29 21:01:16 -0700

    added REST for scheduling rooms

--- src/rest/public/scheduling.js
+++ src/rest/public/scheduling.js
@@ -2,7 +2,9 @@
  * Created by Axiaz on 2017-03-29.
  */
 
-
+var building_distance;
+var target_lat;
+var target_lon;
 
 function scheduling()
 {
@@ -98,14 +100,13 @@ function scheduling()
             emptyFieldAlert();
             return;
         }
-
         roomQuery.WHERE = {IS: {rooms_shortname: buildingVal}};
     }
 
     var dis = formData.get("distanceToggle");
     if (dis == 1) {
 
-        var building_distance = formData.get("meter");
+        building_distance = formData.get("meter");
 
         if (isEmpty(building_distance)) {
             emptyFieldAlert();
@@ -130,8 +131,7 @@ function scheduling()
             emptyFieldAlert();
             return;
         }
-
-        //do your distance thingy here
+        getTargetDis(building_shortname);
     }
 
     console.log(JSON.stringify(courseQuery, null, 4));
@@ -155,8 +155,168 @@ function scheduling()
         console.log(err);
     });
 
+
     var listOfRooms;
+    $.ajax({
+        url: 'http://localhost:63342/query',
+        type: 'POST',
+        data: JSON.stringify(roomQuery),
+        dataType: 'json',
+        crossOrigin: true,
+        cache: false,
+        contentType: 'application/json'
+    }).done( function(data){
+
+        var array = data.result;
+        var typeOfQuery = formData.get("roomQueryType");
+
+        console.log("query room type = " + typeOfQuery);
+        //room query = and
+        if (typeOfQuery == 3)
+        {
+            //FINAL room query for AND
+            listOfRooms = checkDistance(data.result);
+            console.log("FINAL room query for AND:");
+            console.log(listOfRooms);
+        }
+        else if (typeOfQuery == 4)
+        {
+            //query type = OR
+            var query = {
+                "WHERE": {
+                },
+                "OPTIONS": {
+                    "COLUMNS": [
+                        "rooms_fullname", "rooms_shortname", "rooms_number", "rooms_name", "rooms_address", "rooms_lat", "rooms_lon", "rooms_seats", "rooms_type", "rooms_furniture", "rooms_href"
+                    ],
+                    "FORM": "TABLE"
+                }
+            };
+            $.ajax({
+                url: 'http://localhost:63342/query',
+                type: 'POST',
+                data: JSON.stringify(query),
+                dataType: 'json',
+                crossOrigin: true,
+                cache: false,
+                contentType: 'application/json'
+            }).done( function(data){
+                //FINAL room query for OR
+                listOfRooms = mergeArray(roomDistanceFilter(data.result), array);
+                console.log("FINAL room query for OR");
+                console.log(listOfRooms);
+            }).fail( function(err){
+                alert(err.responseText);
+                console.log(err);
+            });
+        }
+    }).fail( function(err){
+        alert(err.responseText);
+        console.log(err);
+    });
 }
+
+
+function roomDistanceFilter(array)
+{
+    console.log("roomDistanceFilter(array) called");
+    var i = 0;
+
+    var array_temp = array.slice();
+
+    $.each(array, function(){
+
+        if (array[i] != null) {
+            var distance = getDis(array[i].rooms_lat, array[i].rooms_lon);
+            console.log(distance + " > " + building_distance);
+            array_temp[i].distance = distance;
+        }
+        i++;
+    });
+    i = 0;
+    $.each(array, function(){
+        if (array[i] != null) {
+            if (array_temp[i].distance > building_distance) {
+                array_temp.splice(i, 1);
+                i--;
+            }
+            i++;
+        }
+    });
+    return array_temp;
+}
+
+
+function mergeArray(array_all, array_unique)
+{
+    var i = 0;
+    $.each(array_unique, function(){
+        if (array_unique[i] != null) {
+            array_unique[i].distance = getDis(array_unique[i].rooms_lat, array_unique[i].rooms_lon);
+        }
+        i++;
+    });
+
+    console.log("mergeArray called");
+    var merged = array_all.concat(array_unique);
+
+
+    i = 0;
+    //remove duplicate
+    $.each(merged, function(){
+        var j = i+1;
+        $.each(merged, function(){
+
+            if(merged[i] != null && merged[j] != null) {
+                if (merged[i].rooms_name == merged[j].rooms_name) {
+                    merged.splice(j--, 1);
+                }
+            }
+            j++;
+        });
+        i++;
+    });
+    return merged;
+}
+
+//For Room AND Query
+function checkDistance(array) {
+    var i = 0;
+    var array_temp = array.slice();
+    $.each(array, function(){
+        if (array[i] != null) {
+            var distance = getDis(array[i].rooms_lat, array[i].rooms_lon);
+            console.log(distance + " > " + building_distance);
+            array_temp[i].distance = distance;
+        }
+        i++;
+    });
+    i = 0;
+    $.each(array, function(){
+
+        if (array[i] != null) {
+            if (array_temp[i].distance > building_distance) {
+                array_temp.splice(i, 1);
+                i--;
+            }
+            i++;
+        }
+    });
+    return array_temp;
+}
+
+
+function getDis(lat1, lon1) {
+    // Math.PI / 180
+    var p = 0.017453292519943295;
+    var cos = Math.cos;
+    var res = 0.5 - cos((target_lat - lat1) * p)/2 +
+        cos(lat1 * p) * cos(target_lat * p) *
+        (1 - cos((target_lon - lon1) * p))/2;
+    // 2 * R; R = 6371 km
+    return 12742 * Math.asin(Math.sqrt(res)) * 1000;
+}
+
 
 
 function generateTable(data, columns) {
@@ -210,4 +370,46 @@ function isEmpty(someField) {
 
 function emptyFieldAlert() {
     alert("Please fill out the field");
+}
+
+
+function getTargetDis(shortname)
+{
+    var query = {
+        "WHERE": {
+        },
+        "OPTIONS": {
+            "COLUMNS": [
+                "rooms_name"
+            ],
+            "FORM": "TABLE"
+        }
+    };
+
+    var array = [];
+    var columns = query.OPTIONS.COLUMNS;
+    array.push({"IS": {"rooms_shortname": shortname}});
+    query.WHERE.AND = array;
+    columns.push("rooms_lat");
+    columns.push("rooms_lon");
+
+    $.ajax({
+        url: 'http://localhost:63342/query',
+        type: 'POST',
+        data: JSON.stringify(query),
+        dataType: 'json',
+        crossOrigin: true,
+        cache: false,
+        contentType: 'application/json'
+    }).done( function(data){
+        //data will be the result json obj
+        console.log('target response: ' + data);
+        console.log(data);
+        console.log('target building lat = ' + data.result[0].rooms_lat + ", lon = " + data.result[0].rooms_lon);
+        target_lat = data.result[0].rooms_lat;
+        target_lon= data.result[0].rooms_lon;
+    }).fail( function(err){
+        alert(err.responseText);
+        console.log(err);
+    });
 }
