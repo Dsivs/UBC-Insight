b773a0fd5a4992f47b627bb062b844a6bb93209c
jpeng06 <awv006@gmail.com>
2017-03-29 17:34:58 -0700

    added REST for scheduling

--- src/rest/public/scheduling.js
+++ src/rest/public/scheduling.js
@@ -84,10 +84,71 @@ function scheduling()
     console.log(JSON.stringify(roomQuery, null, 4));
 
 
-    //TODO
-    var listOfCourses;
+    $.ajax({
+        url: 'http://localhost:63342/query',
+        type: 'POST',
+        data: JSON.stringify(courseQuery),
+        dataType: 'json',
+        crossOrigin: true,
+        cache: false,
+        contentType: 'application/json'
+    }).done( function(data){
+        var listOfCourses = data.result;
+        console.log(listOfCourses);
+        generateTable(listOfCourses, courseQuery.OPTIONS.COLUMNS);
+    }).fail( function(err){
+        alert(err.responseText);
+        console.log(err);
+    });
+
     var listOfRooms;
 
     alert("you want to schedule " + course_dept + "_" + course_num + " in building" + buildingVal + " within "
         + meters +" meters of building " + withinBuilding);
+}
+
+
+function generateTable(data, columns) {
+    var tbl_body = document.createElement("tbody");
+    var odd_even = false;
+    console.log("DATA", data);
+
+    if (data == null || data.length == 0)
+    {
+        alert("No Result Found, Please Try Search Something Else");
+        document.getElementById("tblResults").innerHTML = '';
+        document.getElementById("result").style.display = "none";
+    }
+    else {
+        document.getElementById("result").style.display = "";
+        if ($('#tblResults').children().length > 0)
+        {
+            document.getElementById("tblResults").innerHTML = '';
+        }
+        $.each(data, function () {
+            var tbl_row = tbl_body.insertRow();
+            tbl_row.className = odd_even ? "odd" : "even";
+            $.each(this, function (k, v) {
+                var cell = tbl_row.insertCell();
+                cell.appendChild(document.createTextNode(v.toString()));
+            });
+            odd_even = !odd_even;
+        });
+
+
+        var table = document.getElementById("tblResults");
+        var header = table.createTHead();
+        var row = header.insertRow(0);
+        var i = 0;
+        $.each(columns, function () {
+            var cell = row.insertCell();
+            cell.innerHTML = "<strong>" + columns[i].toString() + "</strong>";
+            i++;
+        });
+
+        //document.getElementById("tblResults").appendChild(tbl_head);
+        document.getElementById("tblResults").appendChild(tbl_body);
+        window.location = "#result";
+        // $("#tblResults").appendChild(tbl_body);
+    }
 }
